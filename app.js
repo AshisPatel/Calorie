96712e4a4b885037c1368a611d3607f88cceb130
AP <ashis.n.patel@gmail.com>
2021-09-16 13:45:37 -0500

    split up macro calc

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const ashis = new User('Ashis', 'male', 150, 12, 66, 25, 'lightly active', 6, 'i
 
 const ashisTarget = ashis.getTargetCals();
 
-console.log(ashis.getMacros(ashisTarget, 1.2, 20));
+console.log( `Maint cals: ${ashis.getMaintCals()}` ,`Target cals: ${ashis.getTargetCals()}`,ashis.getMacros(ashisTarget, 1.6, 20));
 
 const amelia = new User('Amelia', 'female', 152, 23, 69, 25, 'sedentary', 5, 'beginner', 'lose fat'); 
 
